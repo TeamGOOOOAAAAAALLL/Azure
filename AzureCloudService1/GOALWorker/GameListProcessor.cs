474368c095e05d591ff52bd200c65a34bd1801f0
Chris Owen <Chris@ChrisOwen.org>
2015-04-19 08:14:43 -0600

    initial pull from nhl

---
+++ AzureCloudService1/GOALWorker/GameListProcessor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Net;
+
+namespace GOALWorker
+{
+    public class GameListProcessor
+    {
+        public List<Games> GameList { get; set; }
+
+        public void GetGames()
+        {
+            DateTime currentDate = DateTime.Now;
+            string URL = string.Format("http://live.nhle.com/GameData/GCScoreboard/{0}.jsonp", currentDate.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture));
+            WebRequest request = WebRequest.Create(new Uri(URL));
+            var response = request.GetResponse().GetResponseStream();
+            StreamReader reader = new StreamReader(response);
+            var result = reader.ReadToEnd();
+            result = result.Substring(15, result.Length - 17);
+            Debug.Print(result);
+        }
+
+        public void ProcessGameList()
+        {
+
+        }
+    }
+}
---
+++ AzureCloudService1/GOALWorker/Games.cs
@@ -0,0 +1,12 @@
+﻿
+using System;
+
+namespace GOALWorker
+{
+    public class Games
+    {
+        string Identifier { get; set; }
+        public DateTime StartTime { get; set; }
+        bool Active { get; set; }
+    }
+}
