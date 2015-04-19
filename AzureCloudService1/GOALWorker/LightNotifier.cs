698048ab59a773bd9cedac5dc01d74005dad56ad
Jared Simpson <jaredpsimpson@gmail.com>
2015-04-19 15:54:38 -0500

    Send a POST to light up the HockeyGoalLight

--- AzureCloudService1/GOALWorker/LightNotifier.cs
+++ AzureCloudService1/GOALWorker/LightNotifier.cs
@@ -1,4 +1,7 @@
 ﻿using System.Diagnostics;
+using System.Net;
+using System.Text;
+using System.IO;
 
 namespace GOALWorker
 {
@@ -8,6 +11,28 @@ namespace GOALWorker
         public static void NotifyLightsForTeam(string teamname)
         {
             Trace.TraceInformation("GOALWorker is Sending Light Request for {0}", teamname);
+
+            WebRequest request = WebRequest.Create("https://api.spark.io/v1/devices/DEVICEID/score");
+
+            request.Method = "POST";
+
+            string postData = "access_token=ACCESSTOKEN&params=0%2C255%2C0";
+
+            byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+
+            request.ContentType = "application/x-www-form-urlencoded";
+            // Set the ContentLength property of the WebRequest.
+            // Set the ContentLength property of the WebRequest.
+            request.ContentLength = byteArray.Length;
+            // Get the request stream.
+            Stream dataStream = request.GetRequestStream();
+            // Write the data to the request stream.
+            dataStream.Write(byteArray, 0, byteArray.Length);
+            // Close the Stream object.
+            dataStream.Close();
+            // Get the response.
+           request.GetResponse();
+            
         }
     }
 }
